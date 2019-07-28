d7465dce2e23c94fd4df0dcede71dd62ce24b953
nghiepnguyen520 <nghiepnguyenfb@gmail.com>
2019-07-28 11:52:47 +0700

    App

---
+++ precache-manifest.14bddb2598ee43027211d7e4fe6d5afc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca81083ff21dd49e0342023b79ebc5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d72a50b82bc94797bb73",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7315ed163ee9611d07be",
+    "url": "/static/js/2.50f3ffd1.chunk.js"
+  },
+  {
+    "revision": "d72a50b82bc94797bb73",
+    "url": "/static/js/main.97afff92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
